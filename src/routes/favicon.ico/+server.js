0fd9103adbfe1038ce775aefcb74dc233fe55495
TheSevenPens <98374109+TheSevenPens@users.noreply.github.com>
2026-03-08 19:35:05 -0700

    fixed github pages deployment

--- src/routes/favicon.ico/+server.js
+++ src/routes/favicon.ico/+server.js
@@ -1,3 +1,5 @@
+export const prerender = true;
+
 export function GET() {
     // Prevent noisy 404s from browsers requesting /favicon.ico by default.
     return new Response(null, { status: 204 });
